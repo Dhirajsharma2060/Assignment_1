50ed73f2b037af975d1dabffb38d187def103bcc
Dhirajsharma2060 <dhirajsharma2060@gmail.com>
2025-01-15 16:45:44 +0530

    backend services is mostly completed

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, text
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
